15281e223983216ac9b5107f39398052c21047ad
Younous Brayany Houssen <ST10311777@vcconnect.edu.za>
2024-09-08 21:40:05 +0200

    Minor Update
    -Addition of comments to all necessary files

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,42 +1,62 @@
 using CMCSapp_ST10311777.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-
+/// <summary>
+/// Name:           Y.Houssen
+/// Student:        ST10311777
+/// Module code:    PROG6212
+/// </summary>
+/// 
 namespace CMCSapp_ST10311777.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
+        //같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같//
 
-		public HomeController(ILogger<HomeController> logger)
+        private readonly ILogger<HomeController> _logger;
+
+        //같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같//
+        public HomeController(ILogger<HomeController> logger)
 		{
 			_logger = logger;
 		}
 
-		public IActionResult Index()
+        //같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같//
+
+        public IActionResult Index()
 		{
 			return View();
 		}
 
-		public IActionResult Privacy()
+        //같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같//
+
+        public IActionResult Privacy()
 		{
 			return View();
 		}
 
-		public IActionResult LecturerPage()
+        //같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같//
+
+        public IActionResult LecturerPage()
 		{
 			return View();
 		}
 
-		public IActionResult CoordAndManagPage()
+        //같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같//
+
+        public IActionResult CoordAndManagPage()
 		{
 			return View();
 		}
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같//
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
-	}
-}
+
+        //같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같같//
+    }
+}//같같같같같같같같같같같같같같같같같같같같...ooo000 END OF FILE 000ooo...같같같같같같같같같같같같같같같같같같같같//
